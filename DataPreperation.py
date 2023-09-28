a692dd897b4e7e046b3777089d9a941b2994a928
papapiggy <1092882064@qq.com>
2023-09-28 12:22:57 +0800

    Update DataPreperation.py

--- DataPreperation.py
+++ DataPreperation.py
@@ -6,7 +6,7 @@ def divide_to_train_test(data,x_columns,y_column,train_ratio=0.8):
     shuffle(idx)
     train_indexes = idx[:int(len(idx)*train_ratio)]
     test_indexes = idx[int(len(idx) * train_ratio):]
-    X = data[x_columns].as_matrix()
+    X = data[x_columns].values
     Y = data[y_column].values
     return X[train_indexes],Y[train_indexes],X[test_indexes],Y[test_indexes]
 
